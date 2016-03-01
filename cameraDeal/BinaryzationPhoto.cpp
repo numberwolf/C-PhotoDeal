b6502348efb92af4ae8126b6031bc1ddd3f249a6
numberwolf <porschegt23@foxmail.com>
2016-03-01 22:39:41 +0800

    新建

---
+++ cameraDeal/BinaryzationPhoto.cpp
@@ -0,0 +1,9 @@
+//
+//  BinaryzationPhoto.cpp
+//  cameraDeal
+//
+//  Created by numberwolf on 16/3/1.
+//  Copyright © 2016年 numberwolf. All rights reserved.
+//
+
+#include "BinaryzationPhoto.hpp"
---
+++ cameraDeal/BinaryzationPhoto.hpp
@@ -0,0 +1,30 @@
+//
+//  BinaryzationPhoto.hpp
+//  cameraDeal
+//
+//  Created by numberwolf on 16/3/1.
+//  Copyright © 2016年 numberwolf. All rights reserved.
+//
+
+#ifndef BinaryzationPhoto_hpp
+#define BinaryzationPhoto_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+#endif /* BinaryzationPhoto_hpp */
+
+class BinaryzationPhoto {
+    
+public:
+    
+    BinaryzationPhoto() {
+    }
+    
+    ~BinaryzationPhoto() {}
+    
+private:
+    
+protected:
+    
+};
