0d84c8864ba07079e03c1394c7f605f843b53fe4
numberwolf <porschegt23@foxmail.com>
2016-03-09 13:47:46 +0800

    update

--- cameraDeal/numberPhoto.cpp
+++ cameraDeal/numberPhoto.cpp
@@ -15,10 +15,10 @@
 #define PI 3.1416
 
 #define Mask8(x) ( (x) & 0xFF )
-#define R(x) ( Mask8(x) )
-#define G(x) ( Mask8(x >> 8 ) )
-#define B(x) ( Mask8(x >> 16) )
-#define A(x) ( Mask8(x >> 24) )
+#define R(x) ( Mask8(x) )           // 红色通道
+#define G(x) ( Mask8(x >> 8 ) )     // 绿色通道
+#define B(x) ( Mask8(x >> 16) )     // 蓝色通道
+#define A(x) ( Mask8(x >> 24) )     // 透明度通道
 #define RGBAMake(r, g, b, a) ( Mask8(r) | Mask8(g) << 8 | Mask8(b) << 16 | Mask8(a) << 24 )
 
 void numberPhoto::blackAndWhite(uint32_t *pixels, unsigned long width, unsigned long height) {
@@ -50,7 +50,7 @@ void numberPhoto::blackAndWhite(uint32_t *pixels, unsigned long width, unsigned 
     }
      
 //    BinaryzationPhoto::binaryzation(gray_arr, 40, 30, width, height); // 二值
-/*    
+/*    边缘
       BinaryzationPhoto::edgeExamine(gray_arr, 40, 40, width, height);
       BinaryzationPhoto::edgeExamine(temp, 20, 20, width, height);
  */
