9866583d1659f8dac41da1ea2e1665a16dc00e5a
numberwolf <porschegt23@foxmail.com>
2016-03-17 21:03:41 +0800

    更新

--- C-PhotoDeal_framework/Pixels.hpp
+++ C-PhotoDeal_framework/Pixels.hpp
@@ -5,6 +5,20 @@
 //  Created by numberwolf on 16/3/12.
 //  Copyright © 2016年 numberwolf. All rights reserved.
 //
+/**************************************************************************
+ * Email：porschegt23@foxmail.com || numberwolf11@gmail.com
+ * Github:https://github.com/numberwolf
+ * APACHE 2.0 LICENSE
+ * Copyright [2016] [Chang Yanlong]
+ 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ **************************************************************************/
+
 
 #ifndef Pixels_hpp
 #define Pixels_hpp
--- C-PhotoDeal_framework/numberPhoto.hpp
+++ C-PhotoDeal_framework/numberPhoto.hpp
@@ -5,6 +5,19 @@
 //  Created by numberwolf on 16/2/22.
 //  Copyright © 2016年 numberwolf. All rights reserved.
 //  hahah
+/**************************************************************************
+ * Email：porschegt23@foxmail.com || numberwolf11@gmail.com
+ * Github:https://github.com/numberwolf
+ * APACHE 2.0 LICENSE
+ * Copyright [2016] [Chang Yanlong]
+ 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ **************************************************************************/
 
 #ifndef numberPhoto_hpp
 #define numberPhoto_hpp
