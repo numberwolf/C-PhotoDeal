b058d85a105a99e6eba326ec50661332c1c976dd
numberwolf <porschegt23@foxmail.com>
2016-10-14 22:37:48 +0800

    otsu_eg

--- C-PhotoDeal_level2_APP/C-PhotoDeal_level2/main.cpp
+++ C-PhotoDeal_level2_APP/C-PhotoDeal_level2/main.cpp
@@ -15,14 +15,15 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
-    char *path = "/Users/numberwolf/Documents/XCode&C++/C-PhotoDeal/C-PhotoDeal_level2_APP/img/SAE.jpg";
-    canny_door *CannyDoor = new canny_door(path);
+    char *path = "/Users/numberwolf/Documents/XCode&C++/C-PhotoDeal/C-PhotoDeal_level2_APP/img/1.jpg";
+//    canny_door *CannyDoor = new canny_door(path);
     
     IplImage *ColorImage = cvLoadImage(path);
     int height = ColorImage->height;
     int width = ColorImage->width;
     
 //    numberPhoto::sobelCanny(ColorImage, width, height);
+    numberPhoto::otsuBinary(ColorImage, width, height, false, 80, 80);
     
     
     return 0;
